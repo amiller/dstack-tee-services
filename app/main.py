5703fba8a61f12900dd396d69bdeae2fdcba2c77
Andrew Miller <socrates1024@gmail.com>
2024-08-24 13:58:54 -0400

    app invokes rpc

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
+
 import socket
-print(socket.gethostname())
+import subprocess
+
+def latest():
+    cmd = "cast block-number --flashbots"
+    return subprocess.check_output(cmd, shell=True).decode('utf-8')
+
 
 @app.route("/")
 def hello():
-    return "Hello from dstack-app!\nhostname: " + socket.gethostname()
+    def generate():
+        yield "Hello from dstack-app!\n"
+        yield "hostname: " + socket.gethostname() + "\n"
+        yield "cast block latest:" + latest()
+
+    return Response(generate(), mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-set +x
+set -x
 docker build -t dstack-app:latest app
 docker build -t dstack-guest-sim:latest dstack-guest-sim
+minikube kubectl -- delete deployment --all
 minikube kubectl -- apply -f deployment-sim.yaml
 
